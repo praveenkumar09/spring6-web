2daabc853c0c15a7fc8b86fedc118f66feb8c508
praveenkumar09 <tpkprav@gmail.com>
2024-08-13 21:34:55 +0800

    initial commit

---
+++ src/main/java/com/praveen/web/spring6_web/Spring6WebApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.web.spring6_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/web/spring6_web/Spring6WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.web.spring6_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
